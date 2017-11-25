2e17d205dc7cd5de8615823e4d466cb221bf48cf
mister-grumbler <avmeklenko@gmail.com>
2017-11-25 23:51:21 +0300

    Partial implementation of temperature calculation (see issue #1).

--- adc.c
+++ adc.c
@@ -7,6 +7,34 @@
 static unsigned int result;
 static unsigned long averaged;
 
+// The lookup table contains raw ADC values for every degree of Celsius
+// from -52C to 112C.
+const unsigned int rawAdc[] = {
+    974, 971, 967, 964, 960, 956, 953, 948,
+    944, 940, 935, 930, 925, 920, 914, 909,
+    903, 897, 891, 884, 877, 871, 864, 856,
+    849, 841, 833, 825, 817, 809, 800, 791,
+    782, 773, 764, 754, 745, 735, 725, 715,
+    705, 695, 685, 675, 664, 654, 644, 633,
+    623, 612, 601, 591, 580, 570, 559, 549,
+    538, 528, 518, 507, 497, 487, 477, 467,
+    457, 448, 438, 429, 419, 410, 401, 392,
+    383, 375, 366, 358, 349, 341, 333, 326,
+    318, 310, 303, 296, 289, 282, 275, 269,
+    262, 256, 250, 244, 238, 232, 226, 221,
+    215, 210, 205, 200, 195, 191, 186, 181,
+    177, 173, 169, 165, 161, 157, 153, 149,
+    146, 142, 139, 136, 132, 129, 126, 123,
+    120, 117, 115, 112, 109, 107, 104, 102,
+    100, 97, 95, 93, 91, 89, 87, 85,
+    83, 81, 79, 78, 76, 74, 73, 71,
+    69, 68, 67, 65, 64, 62, 61, 60,
+    58, 57, 56, 55, 54, 53, 52, 51,
+    49, 48, 47, 47, 46
+};
+
+#define ADC_RAW_TO_TEMPERATURE_SIZE sizeof rawAdc / 2
+
 /**
  * @brief Initialize ADC's configuration registers.
  */
@@ -45,12 +73,24 @@ unsigned int getAdcAveraged() {
 
 /**
  * @brief Calculation of real temperature using averaged result of
- *  A-D conversion.
+ *  AnalogToDigital conversion and the lookup table.
  * @return temperature in tenth of degrees of Celsius.
  */
 int getTemperature() {
-    return (averaged >> ADC_AVERAGING_BITS)
-        + getParamById(PARAM_TEMPERATURE_CORRECTION) - 163; // for tests only. fix it!
+    unsigned int val = averaged >> ADC_AVERAGING_BITS;
+    unsigned char r = ADC_RAW_TO_TEMPERATURE_SIZE, l = 0;
+
+    while ((r - l) > 1) {
+        unsigned int m = (l + r) >> 1;
+        unsigned int mid = rawAdc[m];
+        if (val > mid) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+
+    return -520 + (r * 10) + getParamById(PARAM_TEMPERATURE_CORRECTION);
 }
 
 /**
@@ -61,7 +101,6 @@ void ADC1_EOC_handler() __interrupt(22) {
     result = ADC_DRH << 2;
     result |= ADC_DRL;
     ADC_CSR &= ~0x80;   // reset EOC
-    result = 512 - result;   // for tests only. remove it!
     // Averaging result
     if(averaged == 0) {
         averaged = result << ADC_AVERAGING_BITS;
