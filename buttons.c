92f1a27f34dcd32b7a1e23571db03622944eeef5
mister-grumbler <avmeklenko@gmail.com>
2017-11-28 01:47:55 +0300

    Relocation of definitions (see issue #8). Populating actual data into the status variable during initialization.

--- buttons.c
+++ buttons.c
@@ -4,16 +4,34 @@
 
 #include "ts.h"
 
+/* Definition for external interrupts */
+// control register 1
+#define EXTI_CR1    *(unsigned char*)0x50A0
+// control register 2
+#define EXTI_CR2    *(unsigned char*)0x50A1
+
+/* Definition for buttons */
+// Port C control input from buttons.
+#define BUTTONS_PORT   PC_IDR
+// PC.3
+#define BUTTON1_BIT    0x08
+// PC.4
+#define BUTTON2_BIT    0x10
+// PC.5
+#define BUTTON3_BIT    0x20
+
 static unsigned char status;
 static unsigned char diff;
 
 /**
- * @brief 
+ * @brief Configure approptiate pins of MCU as digital inputs. Set 
+ *  initial value of status by getting current state of buttons. 
+ *  Configure external interrupt for these inputs.
  */
 void initButtons() {
     PC_CR1 |= BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT;
     PC_CR2 |= BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT;
-    status = 0;
+    status = ~(BUTTONS_PORT & (BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT));
     diff = 0;
     EXTI_CR1 |= 0x30;   // generate interrupt on falling and rising front.
 }
@@ -100,8 +118,8 @@ bool isButton3() {
  */
 void EXTI2_handler() __interrupt(5) {
     unsigned char event;
-    diff = status ^ ~(PC_IDR & (BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT));
-    status = ~(PC_IDR & (BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT));
+    diff = status ^ ~(BUTTONS_PORT & (BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT));
+    status = ~(BUTTONS_PORT & (BUTTON1_BIT | BUTTON2_BIT | BUTTON3_BIT));
     
     // Send appropriate event to menu.
     if (isButton1()) {
